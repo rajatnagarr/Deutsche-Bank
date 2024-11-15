81b0341f4d2435ddfead312c56a70fa062b84824
rajatnagar <rajatnagar@vt.edu>
2024-11-15 06:49:57 -0500

    added

--- futureValue.py
+++ futureValue.py
@@ -14,9 +14,9 @@ def calculate_present_continuous(x, r, n):
     return x / exp(r * n)
 
 if __name__ == '__main__':
-    print(calculate_future_discrete(100, 0.1, 5))
-    print(calculate_future_continuous(100, 0.1, 5))
-    print(calculate_present_discrete(100, 0.1, 5))
-    print(calculate_present_continuous(100, 0.1, 5))
+    print("Future value of $1000 at 5% for 10 years (discrete):", calculate_future_discrete(1000, 0.05, 10))
+    print("Future value of $1000 at 5% for 10 years (continuous):", calculate_future_continuous(1000, 0.05, 10))
+    print("Present value of $1000 at 5% for 10 years (discrete):", calculate_present_discrete(1000, 0.05, 10))
+    print("Present value of $1000 at 5% for 10 years (continuous):", calculate_present_continuous(1000, 0.05, 10))
     
     
